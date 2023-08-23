2c676ceeb27fd495e174d5a275caf597110acb63
a11heand <allheand.master@gmail.com>
2023-08-23 00:57:02 +0000

    Initial commit

---
+++ example.js
@@ -0,0 +1,7 @@
+// function to calculate the area of a rectangle
+function calculateRectangleArea(length, width) {
+  return length * width;
+}
+
+// testing the function with sample values
+console.log(calculateRectangleArea(5, 8)); // output: 40
