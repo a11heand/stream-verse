28c8ba8524b4a4e82e14731b6b5e70cdd6c3341d
a11heand <allheand.master@gmail.com>
2023-08-23 00:58:01 +0000

    Added: calculateSum.js

---
+++ calculateSum.js
@@ -0,0 +1,11 @@
+// filename: calculateSum.js
+
+// Function to calculate the sum of two numbers
+function calculateSum(number1, number2) {
+  return number1 + number2;
+}
+
+// Testing the calculateSum function
+console.log(calculateSum(5, 10)); // Output: 15
+console.log(calculateSum(-3, 7)); // Output: 4
+console.log(calculateSum(0, 0)); // Output: 0
