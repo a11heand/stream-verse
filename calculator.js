f2a0e031fa7fc848f202b15b560557d09970eee7
a11heand <allheand.master@gmail.com>
2023-08-23 15:56:28 +0000

    Added: calculator.js

---
+++ calculator.js
@@ -0,0 +1,21 @@
+/* filename: calculator.js */
+  
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  if (b === 0) {
+    return "Error: Cannot divide by zero";
+  } else {
+    return a / b;
+  }
+}
---
+++ sum.js
@@ -0,0 +1,8 @@
+// filename: sum.js
+// Content: Calculates the sum of two numbers
+
+function sum(a, b) {
+  return a + b;
+}
+
+console.log(sum(5, 10)); // Output: 15
